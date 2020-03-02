fa21af67e52819547691f543d1df0adde0e09d70
kbh0581 <kbh0581@gmail.com>
2020-03-02 21:44:45 +0900

    ADD: 가장 큰 대칭 문자열 찾기

---
+++ src/leetCode/ZigZagConversion.py
@@ -0,0 +1,73 @@
+class Solution:
+
+    #지그재그 출력하기
+    ##
+    '''
+       AYPALISHIRING  IS Reeturn  "PAHNAPLSIIGYIR"
+
+        P   A   H   N
+        A P L S I I G
+        Y   I   R
+
+
+
+    '''
+
+
+
+    def convert(self, s: str, numRows: int) -> str:
+        array = []
+
+        for a in range(numRows):
+            array.append([])
+
+        upFlag = 0
+        downFlag = numRows - 1
+        cursur = 0
+        flag = -1
+        returnStr = ""
+
+        if numRows == 1:
+            return s
+
+        for index in range(len(s)):
+            if cursur == upFlag or cursur == downFlag:
+                flag *= -1
+            array[cursur].append(s[index])
+            cursur += flag
+
+        for line in range(len(array)):
+            returnStr += "".join(array[line])
+
+        return returnStr
+
+
+
+
+
+
+         #   if index % (numRows+1) == 0:
+
+        print(returnStr)
+
+        return array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    Solution.convert(Solution,'PAYPALISHIRING',3)
+
+
+
